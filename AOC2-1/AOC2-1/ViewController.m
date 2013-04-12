9fb81d69abc4ed2a126f094be769439b79153bcf
Nick Stelzer <iamnick@fullsail.edu>
2013-04-11 22:08:28 -0400

    UI Elements added

--- AOC2-1/AOC2-1/ViewController.m
+++ AOC2-1/AOC2-1/ViewController.m
@@ -35,10 +35,51 @@
     [battisonsTrip calculateErrandTime];
     [postOfficeTrip calculateErrandTime];
     
-    // Display Information
-    NSLog(@"You are going to %@ to pick up %d items, it will take approx %d minutes", [weeklyGroceryTrip errandLocation], [weeklyGroceryTrip.groceryList count], [weeklyGroceryTrip errandTimeMinutes]);
-    NSLog(@"You are going to %@ to pick up %d items and drop off %d items. It will take approx %d minutes", [battisonsTrip errandLocation], [battisonsTrip itemsToPickUp], [battisonsTrip itemsToDropOff], [battisonsTrip errandTimeMinutes]);
-    NSLog(@"You are going to %@ to send %d packages and pick up %d packages. It will take approx %d minutes", [postOfficeTrip errandLocation], [postOfficeTrip packagesToSend], [postOfficeTrip packagesToPickUp], [postOfficeTrip errandTimeMinutes]);
+    // Create Labels and Display Information
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 300.0f, 25.0f)];
+    if (titleLabel != nil) {
+    	titleLabel.text = @"Weekly Errand List";
+        titleLabel.textAlignment = NSTextAlignmentCenter;
+        [titleLabel setFont:[UIFont boldSystemFontOfSize:22]];
+        [self.view addSubview:titleLabel];
+    }
+    UILabel *groceryLabelDesc = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 60.0f, 300.0f, 40.0f)];
+    if (groceryLabelDesc != nil) {
+    	groceryLabelDesc.text = [NSString stringWithFormat:@"You need to go to %@ on %@ to pick up %d items.", [weeklyGroceryTrip errandLocation], [weeklyGroceryTrip errandDay], [weeklyGroceryTrip.groceryList count]];
+        groceryLabelDesc.numberOfLines = 2;
+        [self.view addSubview:groceryLabelDesc];
+    }
+    UILabel *groceryLabelTime = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 110.0f, 300.0f, 40.0f)];
+    if (groceryLabelTime != nil) {
+    	groceryLabelTime.text = [NSString stringWithFormat:@"This errand will take approx %d minutes.", [weeklyGroceryTrip errandTimeMinutes]];
+        groceryLabelTime.numberOfLines = 2;
+    	[self.view addSubview:groceryLabelTime];
+    }
+    UILabel *drycleaningLabelDesc = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 180.0f, 300.0f, 60.0f)];
+    if (drycleaningLabelDesc != nil) {
+    	drycleaningLabelDesc.text = [NSString stringWithFormat:@"You need to go to %@ on %@ to drop off %d items and pick up %d items.", [battisonsTrip errandLocation], [battisonsTrip errandDay], [battisonsTrip itemsToDropOff], [battisonsTrip itemsToPickUp]];
+        drycleaningLabelDesc.numberOfLines = 3;
+        [self.view addSubview:drycleaningLabelDesc];
+    }
+    UILabel *drycleaningLabelTime = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 250.0f, 300.0f, 40.0f)];
+    if (drycleaningLabelTime != nil) {
+    	drycleaningLabelTime.text = [NSString stringWithFormat:@"This errand will take approx %d minutes.", [battisonsTrip errandTimeMinutes]];
+        drycleaningLabelTime.numberOfLines = 2;
+        [self.view addSubview:drycleaningLabelTime];
+    }
+    UILabel *mailLabelDesc = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 320.0f, 300.0f, 60.0f)];
+    if (mailLabelDesc != nil) {
+    	mailLabelDesc.text = [NSString stringWithFormat:@"You need to go to the %@ on %@ to send %d packages and pick up %d packages.", [postOfficeTrip errandLocation], [postOfficeTrip errandDay], [postOfficeTrip packagesToSend], [postOfficeTrip packagesToPickUp]];
+        mailLabelDesc.numberOfLines = 3;
+        [self.view addSubview:mailLabelDesc];
+    }
+    UILabel *mailLabelTime = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 390.0f, 300.0f, 40.0f)];
+    if (mailLabelTime != nil) {
+        mailLabelTime.text = [NSString stringWithFormat:@"This errand will take approx %d minutes.", [postOfficeTrip errandTimeMinutes]];
+        mailLabelTime.numberOfLines = 2;
+        [self.view addSubview:mailLabelTime];
+    }
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
